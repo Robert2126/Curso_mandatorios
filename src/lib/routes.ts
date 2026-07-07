5201465070f678d6245484b1b945fa143657224a
Robert2126 <111004854+Robert2126@users.noreply.github.com>
2026-07-07 11:28:54 -0500

    feat: add route constants

---
+++ src/lib/routes.ts
@@ -0,0 +1,10 @@
+export const routes = {
+  login: "/login",
+  home: "/inicio",
+  courses: "/cursos",
+  questions: "/banco-preguntas",
+  mockExams: "/simulacros",
+  progress: "/progreso",
+  reports: "/reportes",
+  admin: "/admin",
+} as const;
